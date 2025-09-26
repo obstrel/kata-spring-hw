c60fc09eb5010bad128fb97ebfe188b1b976f2ca
serge <sergey.xi@yandex.ru>
2025-09-26 13:14:09 +0300

    feat: added KoscheiTheDeathless death

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,24 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Egg6 {
+
+
+    private Needle7 needle;
+
+    public Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
+
+    @Autowired
+    public void setNeedle(Needle7 needle) {
+        this.needle = needle;
+    }
+
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
